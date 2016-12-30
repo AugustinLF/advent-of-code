68a5f5a93373869026a55461b56755046c585dc3
Augustin Le Fèvre <aulefevre@kilix.fr>
2016-12-30 18:09:07 +0000

    7.2

--- 7.js
+++ 7.js
@@ -1,22 +1,32 @@
 //@flow
-// While snooping around the local network of EBHQ, you compile a list of IP addresses (they're IPv7, of course; IPv6 is much too limited). You'd like to figure out which IPs support TLS (transport-layer snooping).
+// You would also like to know which IPs support SSL (super-secret listening).
 
-// An IP supports TLS if it has an Autonomous Bridge Bypass Annotation, or ABBA. An ABBA is any four-character sequence which consists of a pair of two different characters followed by the reverse of that pair, such as xyyx or abba. However, the IP also must not have an ABBA within any hypernet sequences, which are contained by square brackets.
+// An IP supports SSL if it has an Area-Broadcast Accessor, or ABA, anywhere in the supernet sequences (outside any square bracketed sections), and a corresponding Byte Allocation Block, or BAB, anywhere in the hypernet sequences. An ABA is any three-character sequence which consists of the same character twice with a different character between them, such as xyx or aba. A corresponding BAB is the same characters but in reversed positions: yxy and bab, respectively.
 
 // For example:
 
-// abba[mnop]qrst supports TLS (abba outside square brackets).
-// abcd[bddb]xyyx does not support TLS (bddb is within square brackets, even though xyyx is outside square brackets).
-// aaaa[qwer]tyui does not support TLS (aaaa is invalid; the interior characters must be different).
-// ioxxoj[asdfgh]zxcvbn supports TLS (oxxo is outside square brackets, even though it's within a larger string).
-// How many IPs in your puzzle input support TLS?
+// aba[bab]xyz supports SSL (aba outside square brackets with corresponding bab within square brackets).
+// xyx[xyx]xyx does not support SSL (xyx, but no corresponding yxy).
+// aaa[kek]eke supports SSL (eke in supernet with corresponding kek in hypernet; the aaa sequence is not related, because the interior character must be different).
+// zazbz[bzb]cdb supports SSL (zaz has no corresponding aza, but zbz has a corresponding bzb, even though zaz and zbz overlap).
 
 const R = require('ramda');
 
 const isAbba = (s: string, i: number): bool =>
     s[i] === s[i + 3] && s[i + 1] === s[i + 2] && s[i] !== s[i + 1];
 
-// Simply iterate over the string, find an abba, then check if before there is a [ (not a ])
+const getABA = (s: string, i: number): ?string =>
+    s[i] === s[i + 2]
+    && s[i] !== s[i + 1]
+    && s[i] !== '['
+    && s[i] !== ']'
+    && s[i + 1] !== '['
+    && s[i + 1] !== ']'
+        ? s.substr(i, 3)
+        : null;
+
+const getBAB = (aba: string): string => aba[1] + aba[0] + aba[1];
+
 const isIpTLSValid = (ip: string): bool => {
     let isWithinHypernet = false;
     let abba = false;
@@ -39,14 +49,45 @@ const isIpTLSValid = (ip: string): bool => {
     return abba;
 };
 
-const countTLSValid: (input: string) => number = R.pipe(
+const isIpSSLValid = (ip: string): bool => {
+    let isWithinHypernet = false;
+    let abba = false;
+    const ABA = [];
+    const BAB = [];
+
+    for (let i = 0; i < ip.length - 2; i++) {
+        const letter = ip[i];
+
+        if (letter === '[')
+            isWithinHypernet = true;
+        else if (letter === ']')
+            isWithinHypernet = false;
+        else {
+            const aba = getABA(ip, i);
+            if (aba) {
+                (isWithinHypernet ? BAB : ABA).push(aba);
+            }
+        }
+    }
+
+    return ABA.some(aba => BAB.includes(getBAB(aba)));
+};
+
+const countValid = (pred: (s: string) => bool): (s: string) => number => R.pipe(
     R.split('\n'),
-    R.filter(isIpTLSValid),
+    R.filter(pred),
     R.length,
 );
 
+const countTLSValid = countValid(isIpTLSValid);
+const countSSLValid = countValid(isIpSSLValid);
+
 module.exports = {
     isAbba,
+    getABA,
+    getBAB,
     isIpTLSValid,
+    isIpSSLValid,
     countTLSValid,
+    countSSLValid,
 };
--- 7.test.js
+++ 7.test.js
@@ -1,14 +1,34 @@
 const fs = require('fs');
-const {isIpTLSValid, isAbba, countTLSValid} = require('./7');
+const {
+    isIpTLSValid,
+    isAbba,
+    countTLSValid,
+    isIpSSLValid,
+    countSSLValid,
+    getABA,
+    getBAB,
+} = require('./7');
 
 const input = fs.readFileSync('./7.txt', 'utf-8');
 const ex7 = fs.readFileSync('./7ex.txt', 'utf-8');
+const ex72 = fs.readFileSync('./7ex2.txt', 'utf-8');
 
-test('isAbba', () => {
+test('isAbba()', () => {
     expect(isAbba('abba', 0)).toBe(true);
     expect(isAbba('abbar', 1)).toBe(false);
     expect(isAbba('aaaa', 0)).toBe(false);
-})
+});
+
+test('getABA()', () => {
+    expect(getABA('abae', 0)).toBe('aba');
+    expect(getABA('abae', 1)).toBe(null);
+    expect(getABA('aaa', 0)).toBe(null);
+    expect(getABA('a[a', 0)).toBe(null);
+});
+
+test('getBAB()', () => {
+    expect(getBAB('aba')).toBe('bab');
+});
 
 test('isIpTLSValid()', () => {
     expect(isIpTLSValid('abba[mnop]qrst')).toBe(true);
@@ -18,7 +38,19 @@ test('isIpTLSValid()', () => {
     expect(isIpTLSValid('zxabba')).toBe(true);
 });
 
+test('isIpSSLValid', () => {
+    expect(isIpSSLValid('aba[bab]xyz')).toBe(true);
+    expect(isIpSSLValid('xyx[xyx]xyx')).toBe(false);
+    expect(isIpSSLValid('aaa[kek]eke')).toBe(true);
+    expect(isIpSSLValid('zazbz[bzb]cdb')).toBe(true);
+});
+
 test('countTLSValid', () => {
     expect(countTLSValid(ex7)).toBe(2);
     expect(countTLSValid(input)).toBe(105);
+});
+
+test('countSSLValid', () => {
+    expect(countSSLValid(ex72)).toBe(3);
+    expect(countSSLValid(input)).toBe(258);
 });
